b5fda1c475e8811838dddffc57d4afc9c69f7566
Manuel Böhm <manuelboehm1@t-online.de>
2017-03-31 15:56:31 +0200

    #17 make header image a link to home

--- header.php
+++ header.php
@@ -11,10 +11,10 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
-    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
     <?php wp_head(); ?>
 </head>
@@ -23,13 +23,16 @@
 <div id="page" class="site">
 
     <header id="masthead" class="site-header" role="banner">
-        <img class="header-image" src="<?php echo get_template_directory_uri(); ?>/images/gossenpoeten-logo.png"/>
+        <a href="<?php echo esc_url(home_url('/')); ?>">
+            <img class="header-image" src="<?php echo get_template_directory_uri(); ?>/images/gossenpoeten-logo.png"/>
+        </a>
         <nav role="navigation" id="navbar-main">
             <div class="navbar navbar-inverse navbar-static-top">
                 <div class="container">
                     <!-- .navbar-toggle is used as the toggle for collapsed navbar content -->
                     <div class="navbar-header">
-                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse"
+                                data-target=".navbar-responsive-collapse">
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
@@ -43,10 +46,10 @@
                         <?php
                         $args = array(
                             'theme_location' => 'primary',
-                            'depth'      => 2,
-                            'container'  => false,
-                            'menu_class'     => 'nav navbar-nav',
-                            'walker'     => new Bootstrap_Walker_Nav_Menu()
+                            'depth' => 2,
+                            'container' => false,
+                            'menu_class' => 'nav navbar-nav',
+                            'walker' => new Bootstrap_Walker_Nav_Menu()
                         );
                         if (has_nav_menu('primary')) {
                             wp_nav_menu($args);
