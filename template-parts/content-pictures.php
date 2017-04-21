4c3156a173b4177ae000c5585aaf4a5934fa4ced
Samuel Seiz <samuel.seiz@gmail.com>
2017-04-21 13:47:48 +0200

    added changes

--- template-parts/content-pictures.php
+++ template-parts/content-pictures.php
@@ -1,7 +1,7 @@
 <?php
 require_once ('modal.php');
 $args = array (
-'category_name' => 'pictures',
+'category_name' => 'bilder',
 'posts_per_page' => -1,
 'orderby' => 'date',
 );
@@ -13,23 +13,43 @@ if ($cat_posts->have_posts()) : while ($cat_posts->have_posts()) : $cat_posts->t
     <div class="gallery-wrapper">
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-            <?php $media = get_post_gallery_images();
-            the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );?>
-            <img src="<?php the_post_thumbnail_url(); ?>" onclick="openModal(<?php the_ID() ?>);currentSlide(1,<?php the_ID(); ?>)" class="hover-shadow">
+            <?php
+            $media = get_post_gallery(null,false);
+            $mediaIds =explode(",",$media["ids"]);
+            $caption =[];
+            foreach ($mediaIds as $id) {
+                array_push($caption,wp_get_attachment($id));
+            }
+
+            the_title( '<h2 class="entry-title" style="color: #1bb569;">','</h2>' );?>
+            <div class="gallery-thumbnail">
+                <img src="<?php the_post_thumbnail_url(); ?>" onclick="openModal(<?php the_ID() ?>);currentSlide(1,<?php the_ID(); ?>)" class="hover-shadow">
+                <div style="float: left;">
+                    <?php
+                   
+                    $text = wpautop( $post->post_content );
+
+                    $text = strip_shortcodes( $text );
+                    echo $text;
+                    ?>
+                </div>
+            </div>
+
             <div id="myModal<?php the_ID() ?>" class="modal">
                 <span class="close cursor" onclick="closeModal(<?php the_ID(); ?>)">&times;</span>
                 <div class="modal-content">
-                    <?php  $counter2 = 1;
-                    foreach ($media as $value)
+                    <?php  $counter = 1;
+                    foreach ($media["src"] as $value)
                     {
 
                     ?>
                         <div class="mySlides" name="slides<?php the_ID(); ?>">
-                            <div class="numbertext"><?php echo $counter2 ?>/<?php echo count($media)?></div>
+                            <div class="numbertext"><?php echo $counter ?>/<?php echo count($media)?></div>
                             <img src="<?php echo $value; ?>" style="width:100%">
+                            <div class="caption-container"><?php echo $caption[$counter-1]["caption"]; ?></div>
                         </div>
                     <?php
-                        $counter2=$counter2+1;
+                        $counter=$counter+1;
                     }
                     ?>
                     <a class="prev" onclick="plusSlides(-1,<?php the_ID(); ?>)">&#10094;</a>
@@ -41,4 +61,15 @@ if ($cat_posts->have_posts()) : while ($cat_posts->have_posts()) : $cat_posts->t
     </div>
 <?php
 endwhile; endif;
+function wp_get_attachment( $attachment_id ) {
+
+    $attachment = get_post( $attachment_id );
+    return array(
+        'alt' => get_post_meta( $attachment->ID, '_wp_attachment_image_alt', true ),
+        'caption' => $attachment->post_excerpt,
+
+    );
+}
+
+
 ?>
--- template-parts/modal.php
+++ template-parts/modal.php
@@ -21,16 +21,14 @@ function currentSlide(n, postId) {
 function showSlides(n,postId) {
     var i;
     var slides = document.getElementsByName("slides"+postId);
-    var dots = document.getElementsByClassName("demo");
+
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
+
     slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+
 }
 </script>
